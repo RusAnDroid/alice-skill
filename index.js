a584a26b88fb76011e07a19fcc5493a6752403dc
RusAnDroid <an.anarxist@mail.ru>
2020-07-23 06:02:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Для асинхронной работы используется пакет micro.
 const { json } = require('micro');
-var XMLHttpRequest = require('xhr2');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 function toArr(str) {
     let sub = '';
