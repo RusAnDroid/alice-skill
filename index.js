468784fa0db448da9dd042233451e136db966dc0
RusAnDroid <an.anarxist@mail.ru>
2020-07-23 07:23:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -136,8 +136,7 @@ module.exports = async (req, res) => {
                     if (!usd_flag) {
                         sum = tokens_arr[token_pos - 1];
                     } else {
-                        sum = tokens_arr[token_pos];
-                        sum.pop();
+                        sum = request.nlu.tokens[token_pos];
                     }
                     response_text = 'Учитывая среднестатистическую цену девяностограммового дошика (35 рублей), и курс USD к RUB по данным ЦБ РФ (1$ - ' + usd_coef + ' ';
                     response_tts = 'учи+тывая средн+е статист+ическую цену девян+о стограм+ового дошика <[ d oo sh i k a ]> sil <[270]> 35 рубл+ей sil <[350]> и курс д+оллара к рубл+ю по д+анным центр+ального б+анка эр эф sil <[270]> од+ин д+оллар sil <[500]> ' + usd_coef + ' ';
