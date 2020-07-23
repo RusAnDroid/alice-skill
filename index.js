291e96bd426a856e007c2ce8911f8a19d001571f
RusAnDroid <an.anarxist@mail.ru>
2020-07-23 08:19:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -82,16 +82,16 @@ module.exports = async (req, res) => {
                     }
 
                     if (token_pos == 0 && !usd_flag) {
-                        response_text = dont_understand_text;
+                        response_text = dont_understand_text + 0;
                         response_tts = dont_understand_tts;
                     } else if (!usd_in && !eur_in && !rub_in) {
-                        response_text = dont_understand_text;
+                        response_text = dont_understand_text + 1;
                         response_tts = dont_understand_tts;
                     } else if (usd_in && eur_in || usd_in && rub_in || eur_in && rub_in) {
-                        response_text = dont_understand_text;
+                        response_text = dont_understand_text + 2;
                         response_tts = dont_understand_tts;
                     } else if (tokens_arr[token_pos - 1].replace(/\s/g, '').length === 0 || isNaN(tokens_arr[token_pos - 1])) {
-                        response_text = dont_understand_text;
+                        response_text = dont_understand_text + 3;
                         response_tts = dont_understand_tts;
                     } else if (rub_in) {
                         let sum = tokens_arr[token_pos - 1];
